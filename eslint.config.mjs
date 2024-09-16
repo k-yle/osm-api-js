860d422ed88ce3fcf53eaeb3d07c44eaf8ad747f
Kyle Hensel <k.y.l.e@outlook.co.nz>
2024-09-16 22:53:35 +1000

    update devDeps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,5 @@ import tsEslint from "typescript-eslint";
 import config from "eslint-config-kyle";
 
 export default tsEslint.config(...config, {
-  ignores: ["**/*.snap"],
   rules: { quotes: ["error", "double"] },
 });
